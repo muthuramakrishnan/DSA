2f1db27cd2ffddb81ad3bf49e6bb0d0c6148a8c4
muthuramakrishnan <74167303+muthuramakrishnan@users.noreply.github.com>
2022-01-24 18:09:36 +0530

    sum of all sub arrays optimized - matrix

---
+++ Optimized_SumofAllSubArraysMatrix.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int m, n;
+	cin >> m >> n;
+
+	int arr[m][n];
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+	int sum = 0;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			sum += ((i + 1) * (j + 1) * (m - i) * (n - j)) * arr[i][j];
+		}
+	}
+
+	cout << sum;
+}
